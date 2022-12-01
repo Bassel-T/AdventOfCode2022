f8477a2a454f6c71226169fb917e4864b5e8c817
Bassel-T <lessab99@gmail.com>
2022-12-01 07:55:07 -0600

    Day 1

---
+++ AdventOfCode2022/Day1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace AdventOfCode2022 {
+	public static class Day1 {
+		public static void Part1() {
+			var text = File.ReadAllLines("input.txt");
+			var max = 0;
+			var curr = 0;
+			foreach (var line in text) {
+				if (string.Empty == line) {
+					if (max < curr) {
+						max = curr;
+					}
+					curr = 0;
+				} else {
+					curr += int.Parse(line);
+				}
+			}
+
+			Console.WriteLine(max);
+		}
+
+		public static void Part2() {
+			var text = File.ReadAllLines("input.txt");
+			List<int> carries = new List<int>();
+			var curr = 0;
+			foreach (var line in text) {
+				if (string.Empty == line) {
+					carries.Add(curr);
+					curr = 0;
+				} else {
+					curr += int.Parse(line);
+				}
+			}
+
+			var max = carries.OrderByDescending(x => x).Take(3).Sum();
+			Console.WriteLine(max);
+		}
+	}
+}
---
+++ AdventOfCode2022/Program.cs
@@ -0,0 +1,4 @@
+﻿using AdventOfCode2022;
+
+Day1.Part1();
+Day1.Part2();
