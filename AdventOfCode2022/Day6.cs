25a5050f8d08e868829312ec2e4cf96ea0d3b15e
Bassel-T <lessab99@gmail.com>
2022-12-05 23:07:32 -0600

    Day 6

---
+++ AdventOfCode2022/Day6.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2022 {
+	public static class Day6 {
+
+		public static void Part1() {
+			var text = File.ReadAllText("Input.txt").ToCharArray();
+			for (int i = 3; i < text.Length; i++) {
+				var span = text.Skip(i - 3).Take(4).Distinct().Count();
+				if (span == 4) {
+					Console.WriteLine(i + 1);
+					break;
+				}
+			}
+		}
+
+		public static void Part2() {
+			var text = File.ReadAllText("Input.txt").ToCharArray();
+			for (int i = 13; i < text.Length; i++) {
+				var span = text.Skip(i - 13).Take(14).Distinct().Count();
+				if (span == 14) {
+					Console.WriteLine(i + 1);
+					break;
+				}
+			}
+		}
+
+	}
+}
